852e9e68965f743beaf8624bf4a075fc542fc7b7
evandahs15 <vandahs@outlook.com>
2019-06-24 23:16:07 +1200

    added complex data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import AddAppointments from './AddAppointments'
+import SearchAppointments from './SearchAppointments'
+import ListAppointments from './ListAppointments'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      myAppointments: []
+    };
+  }
+
+componentDidMount(){
+  fetch('./data.json')
+    .then(response => response.json())
+    .then(result => {
+      const apts = result.map(item => {
+        return item
+      })
+      this.setState({
+        myAppointments: apts
+      })
+    })
+}
+
   render() {
     return (
       <main className="page bg-white" id="petratings">
@@ -9,9 +32,9 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <div>Add Appointments</div>
-                <div>Search Appointments</div>
-                <div>List Appointments</div>
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
               </div>
             </div>
           </div>
